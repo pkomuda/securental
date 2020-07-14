eb087eda10a039aea78963c385cd63573e0c3f19
216802 Komuda Przemysław <216802@edu.p.lodz.pl>
2020-07-14 13:38:43 +0200

    Initialize backend project

---
+++ backend/src/main/java/pl/lodz/p/it/securental/SecurentalApplication.java
@@ -0,0 +1,12 @@
+package pl.lodz.p.it.securental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurentalApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/pl/lodz/p/it/securental/SecurentalApplicationTests.java
@@ -0,0 +1,12 @@
+package pl.lodz.p.it.securental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
