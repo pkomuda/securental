f0fa099d1547c6ff652a7696042ba24e47efe24d
216802 Komuda Przemysław <216802@edu.p.lodz.pl>
2020-07-15 14:11:19 +0200

    Install necessary packages, create placeholder pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+    render() {
+        return (
+            <React.Fragment>
+                <Router>
+                    <Container>
+                        <Switch>
+                            <Route exact path="/" component={Home}/>
+                            <Route component={NotFound}/>
+                        </Switch>
+                    </Container>
+                </Router>
+            </React.Fragment>
+        );
+    }
 }
-
-export default App;
