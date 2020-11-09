49c23f94fdbd76e42256b52ae3a348d8b286d358
216802 Komuda Przemysław <216802@edu.p.lodz.pl>
2020-11-10 00:19:52 +0100

    Przypadki użycia

--- backend/src/main/java/pl/lodz/p/it/securental/Main.java
+++ backend/src/main/java/pl/lodz/p/it/securental/Main.java
@@ -1,17 +1,10 @@
 package pl.lodz.p.it.securental;
 
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
 import java.util.List;
-
-import static pl.lodz.p.it.securental.security.CustomUserDetailsService.getUserRoles;
 
 public class Main {
 
     public static void main(String[] args) {
         List<String> groups = List.of("ADMIN", "EMPLOYEE");
-        for (SimpleGrantedAuthority authority : getUserRoles(groups)) {
-            System.out.println(authority);
-        }
     }
 }
